aa26220c1e2f7b147c4f60802fb67c463f07961d
yinhy02 <415150806@qq.com>
2019-11-05 23:44:19 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const { series, src, dest, watch } = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const rename = require('gulp-rename');
+
+// create readable css from scss files
+function sassTask() {
+    return src(['src/**/*.scss', 'src/**/*.css', '!src/**/_*.scss', '!src/var.scss'])
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(dest('dist/'))
+}
+// create minified scss files
+function sassMinTask(){
+    return src(['src/**/*.scss', 'src/**/*.css', '!src/**/_*.scss', '!src/var.scss'])
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+            return path;
+        }))
+        .pipe(dest('dist/'))
+}
+// delete dist folder
+function cleanTask(){
+    return del(['dist']);
+}
+
+// start development watch scss change
+exports.default = exports.dev = series(sassTask, function(){
+    watch('src/**/*.scss', sassTask)
+})
+exports.clean = cleanTask;
+exports.build = series(cleanTask, sassTask, sassMinTask)
